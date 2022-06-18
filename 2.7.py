98def6bb84b2466179d056a91820fb2f0ea972a5
Alex <alex.martinez2017@alu.uct.cl>
2022-06-17 21:06:35 -0400

    Listo el 2.7

--- 2.7.py
+++ 2.7.py
@@ -8,9 +8,12 @@ eta = 0.000001
 inputpunto = input('Ingrese un Punto (x,y) separado unicamente por una coma: ').split(',')
 aPunto = Matrix([[inputpunto[0]],[inputpunto[1]]])
 grad = Matrix([[sp.diff(f,x)],[sp.diff(f,y)]])
+a=[]
+
 while(float(grad.subs([(x, aPunto[0]), (y, aPunto[1])]).norm()) >= eta):
     delx = -grad.subs([(x, aPunto[0]), (y, aPunto[1])])
     aPunto = aPunto + t*delx
-    print(aPunto)
+    a.append(aPunto)
+
 
 print ('El punto optimo en la funcion ',f,', es: ('+str(aPunto[0])+','+ str(aPunto[1])+ ')')
